9125570955b31ea7ff396b2840f0a63cb722b076
Skyler <1056375224@qq.com>
2020-03-05 18:27:10 +0800

    领域基础 引用对象和值对象

---
+++ Skr3D.Domain/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using Skr3D.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skr3D.Domain.Models
+{
+    public class Order : Entity
+    {
+        protected Order()
+        {
+        }
+        public Order(Guid id, string name,Address address, List<OrderItem> orderItem)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            OrderItem = orderItem;
+        }
+        /// <summary>
+        /// 订单名
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// 收获地址
+        /// </summary>
+        public Address Address { get; private set; }
+
+        /// <summary>
+        /// 订单详情
+        /// </summary>
+        public virtual ICollection<OrderItem> OrderItem { get; private set; }
+
+
+
+    }
+}
---
+++ Skr3D.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Skr3D.Domain.Core.Models;
+
+namespace Skr3D.Domain.Models
+{
+    public class OrderItem : Entity
+    {
+
+        /// <summary>
+        /// 详情名
+        /// </summary>
+        public string Name { get; private set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
